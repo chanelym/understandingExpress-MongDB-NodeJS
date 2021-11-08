f6598cd3c70708967055163463323e29547b992e
chanelym <chanelym@gmail.com>
2021-11-08 18:12:05 -0300

    Env configurations

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function Conn(){
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fpxtn.mongodb.net/test`, {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_BASE}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => {
